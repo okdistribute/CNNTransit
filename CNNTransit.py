607667f54e40db9066fca1bebcab9f918f49bb9e
Karissa McKelvey <krmckelv@gmail.com>
2012-12-02 15:52:10 -0500

    Fleshed out basic cnn transit program with tests.

--- CNNTransit.py
+++ CNNTransit.py
@@ -8,7 +8,14 @@ This traverses the cnn transcripts.
 """
 
 from nltk import bigrams
+from dateutil.parser import *
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import os
+import unittest
+from forkmap import *
+from multiprocessing import Pool
+
 
 class CNNTransit(object):
     """
@@ -21,15 +28,20 @@ class CNNTransit(object):
 
     Example: Get transcripts containing Barack Obama and Mitt Romney
 
-    names = [BarackObama, MittRomney]
-
+    names = ["Barack Obama", "Mitt Romney"]
     ds = CNNTransit("election2012")
-    ds.filter = lambda w : w in names
-    ds.write = lambda t, w : 
-    def finish():
-        print "All done after %d" % datetime.now() - start_time
-    ds.finish = finish
+    counts = {}
 
+    def filter(self, text):
+        for bigram in bigrams(text):
+            if bigram in names:
+                counts[bigram] += 1
+
+    def finish(self):
+        print counts
+
+    ds.filter = filter
+    ds.finish = finish
     ds.collect(path="/path/to/cnn/TRANSCRIPTS", 
                start="2012-08-01", 
                end="2012-11-01")
@@ -38,30 +50,65 @@ class CNNTransit(object):
     def __init__(self, name):
         pass
 
-    def filter(self, transcript):
-        """Returns True if the transcript should be included in the dataset."""
-        return True
-
-    def write(self, transcript):
-        """How the transcript should be written"""
+    def filter(self, text):
+        """Given the text of each transcript, do what you will"""
         pass
 
     def finish(self):
-        """What to do when we are finished"""
+        """what to do at the end"""
         pass
+
+    @parallelizable(15,perproc=4)
+    def do_filter(self, p):
+        o = open(p)
+        bs = BeautifulSoup(o)
+        text = bs.get_text()
+        self.filter(text)
+        o.close()
 
     def collect(self, path=None, start=None, end=None):
-        """Collects the transcripts"""
-        pass
+        """Collects the transcripts. Dates are inclusive
 
-class Transcript(object):
-    """
-    Defines a transcript object
-    """
+        """
 
-    def __init__(self, path):
-        pass
+        start = parse(start)
+        end = parse(end)
+        offset = timedelta(days=1)
+        programs = []
+
+        cur = start
+        while cur <= end:
+            datestr = "%02d%02d" % (int(str(cur.year)[2:]), cur.month)
+
+            current_path = os.path.join(path, datestr, "%02d" % cur.day)
+            htmls = os.listdir(current_path)
+            htmls = map(lambda p: os.path.join(current_path, p), htmls)
+            programs += htmls
+
+            cur += offset
+
+        map(self.do_filter, programs)
+        self.finish()
+
+
+class TestCNNTransit(unittest.TestCase):
+
+    def setUp(self):
+        self.cnn = CNNTransit("transit")
+
+        def filter(text):
+            print text[:3]
+            return True
+
+        def finish():
+            print "done"
+
+        self.cnn.filter = filter
+        self.cnn.finish = finish
+
+    def test_small(self):
+        self.cnn.collect(path="/Users/kmac/data/TRANSCRIPTS", start="2010-02-08", end="2010-02-10")
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    unittest.main()
+
