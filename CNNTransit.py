e7ddfed014580b70a02b2877374311b3660e58bc
Karissa McKelvey <krmckelv@gmail.com>
2012-12-19 11:22:19 -0500

    added date as an argument to the filter

--- CNNTransit.py
+++ CNNTransit.py
@@ -31,12 +31,12 @@ class CNN(object):
     ds = CNNTransit("election2012")
     counts = {}
 
-    def filter(self, text):
+    def filter(date, text):
         for bigram in bigrams(text):
             if bigram in names:
                 counts[bigram] += 1
 
-    def finish(self):
+    def finish():
         print counts
 
     ds.filter = filter
@@ -49,25 +49,26 @@ class CNN(object):
     def __init__(self, name):
         pass
 
-    def filter(self, text):
-        """Given the text of each transcript, do what you will"""
+    def filter(self, date, text):
+        """Given the date and text of each transcript, do what you will"""
         pass
 
     def finish(self):
         """what to do at the end"""
         pass
 
-    @parallelizable(15,perproc=4)
+    #@parallelizable(15,perproc=4)
     def do_filter(self, p):
-        o = open(p)
-        bs = BeautifulSoup(o)
-        text = bs.get_text()
-        self.filter(text)
+        date = p[0]
+        path = p[1]
+        o = open(path)
+        bs = BeautifulSoup(o, "lxml")
+        text = bs.body.get_text()
+        self.filter(date, text)
         o.close()
 
     def collect(self, path=None, start=None, end=None):
-        """Collects the transcripts. Dates are inclusive
-        """
+        """Collects the transcripts. Dates are inclusive"""
 
         start = parse(start)
         end = parse(end)
@@ -79,9 +80,9 @@ class CNN(object):
             datestr = "%02d%02d" % (int(str(cur.year)[2:]), cur.month)
 
             current_path = os.path.join(path, datestr, "%02d" % cur.day)
-            htmls = os.listdir(current_path)
-            htmls = map(lambda p: os.path.join(current_path, p), htmls)
-            programs += htmls
+            paths = os.listdir(current_path)
+            paths = map(lambda p: os.path.join(current_path, p), htmls)
+            programs += [parse(datestr), paths]
 
             cur += offset
 
@@ -94,7 +95,7 @@ class TestCNNTransit(unittest.TestCase):
     def setUp(self):
         self.cnn = CNN("transit")
 
-        def filter(text):
+        def filter(date, text):
             print "filtering"
             return True
 
